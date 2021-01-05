046f19f9ff82a43b1ed08d43394980dc9dcdef8e
Rivan Henrique Bello <rivanbello@gmail.com>
2021-01-05 08:53:31 -0300

    fc

--- src/controllers/OgtagController.ts
+++ src/controllers/OgtagController.ts
@@ -18,16 +18,16 @@ export default class OgtagController {
       });
       response.write(
         `${
-          `${
-            `${
-              '<!DOCTYPE html>' +
-              '<html lang="pt-br">' +
-              '<head>' +
-              '<meta charset="UTF-8">' +
-              '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
-              '<meta property="og:title" content="'
-            }${ogtag?.title}" />` + `<meta property="og:description" content="`
-          }${ogtag?.description}" />` +
+          `${`${
+            '<!DOCTYPE html>' +
+            '<html lang="pt-br">' +
+            '<head><title>ogtag</title>' +
+            '<meta charset="UTF-8">' +
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+            '<meta property="og:title" content="'
+          }${ogtag?.title}" /><meta property="og:description" content="`}${
+            ogtag?.description
+          }" />` +
           `<meta property="og:image" content="${ogtag?.image}" />` +
           `</head>` +
           `<body>` +

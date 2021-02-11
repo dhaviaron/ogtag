3f80be37d069b8d9915815833aa509a37bccc663
Rivan Henrique Bello <rivanbello@gmail.com>
2021-02-11 19:34:49 -0300

    Update IOgtagRepository.ts

--- src/repositories/IOgtagRepository.ts
+++ src/repositories/IOgtagRepository.ts
@@ -1,6 +1,7 @@
 import IOgtagDTO from '../dtos/IOgtagDTO';
 import Ogtag from '../schemas/Ogtag';
 
+// interface
 export default interface IOgtagRepository {
   create(data: IOgtagDTO): Promise<Ogtag>;
   get(hash: string): Promise<Ogtag | undefined>;

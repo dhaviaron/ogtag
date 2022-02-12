374f791d0fa3973f1cea097bdcc15963a4f50006
Rivan Bello <8796097+rivanbello@users.noreply.github.com>
2022-02-11 21:07:27 -0300

    add

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -6,7 +6,14 @@ import routes from './routes';
 
 import '../container';
 
-createConnections();
+createConnections([
+  {
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    database: 'ogtag',
+  },
+]);
 
 const app = express();
 
